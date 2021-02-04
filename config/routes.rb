fe1d8ba8b1bac549519214756ad87e4c452d2a0b
Blaine Love <blainelove94@gmail.com>
2021-02-04 15:17:07 -0500

    pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   
+  resources :songs
+
+  get "/artists", to: "artists#index", as: "artists"
+  get "/artists/new", to: "artists#new", as: "new_artist"
+  post "/artists", to: "artist#create"
+  get "/artists/:id", to: "artists#show", as: "artist"
+  get "/artists/:id/edit", to: "artists#edit", as: "edit_artist"
+  patch "/artists/:id", to: "artists#update"
+  delete "/artists/:id", to: "artists#destroy"
+
+
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
-Artist.create(name: "Michael Jackson", age: rand(50), title: "King of Pop")
-Artist.create(name: "Stevie Wonder", age: rand(50), title: "Little Stevie Wonder")
-Artist.create(name: "Elvis Presley", age: rand(50), title: "the King")
-Artist.create(name: "Whitney Houston", age: rand(50), title: "The Voice")
-Artist.create(name: "Beyoncé", age: rand(50), title: "Destiny's Child")
+jackson = Artist.create(name: "Michael Jackson", age: rand(50), title: "King of Pop")
+stevie = Artist.create(name: "Stevie Wonder", age: rand(50), title: "Little Stevie Wonder")
+elvis = Artist.create(name: "Elvis Presley", age: rand(50), title: "the King")
+whitney = Artist.create(name: "Whitney Houston", age: rand(50), title: "The Voice")
+beyonce = Artist.create(name: "Beyoncé", age: rand(50), title: "Destiny's Child")
 
-Instrument.create(name: "Guitar", classification: "Strings")
-Instrument.create(name: "Violin", classification: "Strings")
-Instrument.create(name: "Flute", classification: "Woodwind")
-Instrument.create(name: "Xylophone", classification: "Percussion")
+guitar = Instrument.create(name: "Guitar", classification: "Strings")
+violin = Instrument.create(name: "Violin", classification: "Strings")
+flute = Instrument.create(name: "Flute", classification: "Woodwind")
+xylophone = Instrument.create(name: "Xylophone", classification: "Percussion")
+
+s1 = Song.create(name: "thriller", genre: "pop", artist_id: jackson.id, instrument_id: guitar.id)
+s2 = Song.create(name: "single ladies", genre: "pop", artist_id: beyonce.id, instrument_id: violin.id)
+s3 = Song.create(name: "hound dog", genre: "pop", artist_id: elvis.id, instrument_id: guitar.id)
+s4 = Song.create(name: "Isn't she lovley", genre: "soul", artist_id: stevie.id, instrument_id: violin.id)
+s5 = Song.create(name: "beat it", genre: "pop", artist_id: jackson.id, instrument_id: xylophone.id)
+
+puts "Seeded"
